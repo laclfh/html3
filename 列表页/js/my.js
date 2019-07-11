c21ae2fb4a6654b36a561fe01c382112349ea0c5
laclfh <1271537629@qq.com>
2019-07-11 10:11:45 +0800

    Add files via upload

---
+++ 列表页/js/my.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+$(document).ready(function(){
+  $(".wx").mouseenter(function(){
+    $(".wx_ewm").show();
+  });
+  $(".wx").mouseleave(function(){
+    $(".wx_ewm").hide();
+  });
+});
+
+
+$(document).ready(function(){
+  $(".wb").mouseenter(function(){
+    $(".wb_ewm").show();
+  });
+  $(".wb").mouseleave(function(){
+    $(".wb_ewm").hide();
+  });
+});
+
+
+
